3126b11ca55de70cbb78589834f61c344f952973
Uri Fuholichev <Uri@Uris-Mac-mini.local>
2016-10-07 17:30:47 +0300

    0.0

---
+++ MapKitExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapKitExample
+//
+//  Created by Uri Fuholichev on 10/7/16.
+//  Copyright © 2016 Andrei Karpenia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapKitExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapKitExample
+//
+//  Created by Uri Fuholichev on 10/7/16.
+//  Copyright © 2016 Andrei Karpenia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
