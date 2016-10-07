8fb44ec60770d2bee5ac949a84f53802aa0fb141
Uri Fuholichev <Uri@Uris-Mac-mini.local>
2016-10-07 19:34:10 +0300

    all works

--- MapKitExample/ViewController.h
+++ MapKitExample/ViewController.h
@@ -7,9 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CLLocationManagerDelegate, MKMapViewDelegate>
 
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
 
+/*
+ 1) Небольшой ридми. Итак, для начала не забудь добавить в plist
+     NSLocationWhenInUseUsageDescription
+     NSLocationAlwaysUsageDescription
+ Это чтобы иметь возможность эмулировать точки. Дальше. Импортируем МапКит и КорЛокэйшн фрэймворки. 
+ 2) На всякий случай я еще добавил Allow arbitrary loads в plist, чтобы иметь доступ к интернету.
+ 3) В сториборде добавляем на ВьюКонтроллер MapView. C этого момента, даже если не сделать оутлет, все будет работать.То есть в симуляторе будет показываться карта.
+ 4) Делаем оутлет МапВью (ну это, если конечно не добавляли его программно);
+ 5) Создаем объект Локайшн Менежер, и там все что записано во ВьюДидЛоад;
+ 6) Выставляем делегата МапВью;
+*/
--- MapKitExample/ViewController.m
+++ MapKitExample/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    //Этот менеджер - ключевая фишка, чтобы отлавливать геоданные
+    CLLocationManager *locationManager;
+}
 
 @end
 
@@ -16,12 +20,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //не заубудь выставить делегата для мапВью
+    self.mapView.delegate = self;
+    
+    locationManager = [[CLLocationManager alloc] init];
+    [locationManager requestWhenInUseAuthorization];
+    [locationManager startUpdatingLocation];
+//    locationManager.delegate = self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    //здесь мы делаем zoom над позицией пользователя, стороны прямоугольника 800х800 метров
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
+    [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
+    
+    // Add an annotation: добавление аннотации. Здесь мы добавили булавку на карту, булавка соответствует стартовому положению пользователя. На булавку можно нажать и выскочит описание.
+    MKPointAnnotation *point = [[MKPointAnnotation alloc] init];
+    point.coordinate = userLocation.coordinate;
+    point.title = @"Where am I?";
+    point.subtitle = @"I'm here!!!";
+    
+    [self.mapView addAnnotation:point];
 }
 
 @end
